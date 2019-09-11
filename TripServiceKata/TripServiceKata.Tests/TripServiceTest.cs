0d1ffd1b6c3f27dbeeddcbabc4286babc12cfa0e
Ron Heywood <ron.heywood@spektrix.com>
2019-09-11 11:07:21 +0100

    Kata to demonstrate approach to refactoring legacy code from https://github.com/sandromancuso/trip-service-kata/

---
+++ TripServiceKata/TripServiceKata.Tests/TripServiceTest.cs
@@ -0,0 +1,13 @@
+﻿using NUnit.Framework;
+namespace TripServiceKata.Tests
+{
+    public class TripServiceTest
+    {
+        [NUnit.Framework.Test]
+        public void Should_()
+        {
+            var answer = 42;
+            Assert.That(answer, Is.EqualTo(42), "Some useful error message");
+        }
+    }
+}
---
+++ TripServiceKata/TripServiceKata/Exception/UserNotLoggedInException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TripServiceKata.Exception
+{
+    [Serializable]
+    public class UserNotLoggedInException : System.Exception
+    {
+        
+    }
+}
